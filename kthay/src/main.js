a9c7a4fa7feff351a704183f9ab6c852efe91787
kai <kaihay168@gmail.com>
2024-03-18 18:10:04 -0700

    heh

--- kthay/src/main.js
+++ kthay/src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import imagep from '/public/gram.jpg';
+import imagep from './gram.jpg';
 //CAMERA AND SCENE
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
